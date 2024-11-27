97bd3877e9f97fc59909d5e3afc56001126e01a0
Rodrigo L Ribeiro <rlr.para@gmail.com>
2024-11-26 22:13:45 -0300

    ajustes

--- AppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ AppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,5 +1,54 @@
-﻿namespace AppMinhasCompras.Helpers;
+﻿using AppMinhasCompras.Models;
+using SQLite;
+using System.Text;
+
+namespace AppMinhasCompras.Helpers;
 
 public class SQLiteDatabaseHelper
 {
+    private readonly SQLiteAsyncConnection _connection;
+
+    public SQLiteDatabaseHelper(string path)
+    {
+        _connection = new SQLiteAsyncConnection(path);
+        _connection.CreateTableAsync<Produto>().Wait();
+    }
+
+    public Task<int> Insert(Produto produto)
+    {
+        return _connection.InsertAsync(produto);
+    }
+    public Task<List<Produto>> Update(Produto produto)
+    {
+        var sqlPesquisa = new StringBuilder();
+
+        sqlPesquisa.AppendLine($"UPDATE Produto");
+        sqlPesquisa.AppendLine($"   SET Descricao=?,");
+        sqlPesquisa.AppendLine($"       Quantidade=?,");
+        sqlPesquisa.AppendLine($"       Preco=?");
+        sqlPesquisa.AppendLine($" WHERE Codigo=?;");
+
+        return _connection.QueryAsync<Produto>(sqlPesquisa.ToString(), produto.Descricao, produto.Quantidade, produto.Preco, produto.Codigo);
+    }
+    public Task<int> Delete(int codigo)
+    {
+        return _connection.Table<Produto>().DeleteAsync(x => x.Codigo == codigo);
+    }
+    public Task<List<Produto>> GetAll()
+    {
+        return _connection.Table<Produto>().ToListAsync();
+    }
+    public Task<List<Produto>> Search(string? query)
+    {
+        var sqlPesquisa = new StringBuilder();
+
+        sqlPesquisa.AppendLine($"SELECT Codigo,");
+        sqlPesquisa.AppendLine($"       Descricao,");
+        sqlPesquisa.AppendLine($"       Preco,");
+        sqlPesquisa.AppendLine($"       Quantidade");
+        sqlPesquisa.AppendLine($"  FROM Produto");
+        sqlPesquisa.AppendLine($" WHERE Descricao like '%{query}%'");
+
+        return _connection.QueryAsync<Produto>(sqlPesquisa.ToString());
+    }
 }
