de0e7cf8899f1ca36eb920c4b6733642cbf023ea
Rodrigo L Ribeiro <rlr.para@gmail.com>
2024-11-26 21:45:22 -0300

    ajustes

--- AppMinhasCompras/Models/Produto.cs
+++ AppMinhasCompras/Models/Produto.cs
@@ -1,5 +1,12 @@
-﻿namespace AppMinhasCompras.Models;
+﻿using SQLite;
+
+namespace AppMinhasCompras.Models;
 
 public class Produto
 {
+    [PrimaryKey, AutoIncrement]
+    public int Codigo { get; set; }
+    public string? Descricao { get; set; }
+    public double Preco { get; set; }
+    public double Quantidade { get; set; }
 }
